515f7eca05e785f87b19fa41866264210428d9de
Alexey <itmorze@gmail.com>
2013-02-19 22:42:40 +0400

    first upload

--- MSIPClassLibrary/MSIPClassLibrary/Class1.cs
+++ MSIPClassLibrary/MSIPClassLibrary/Class1.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace MSIPClassLibrary
 {
-    public class Class1
+    public class User
     {
+        private string _displayName;
+        private string _userID;
+        private string _domain;
+        private string _serverport;
+        private string _password;
+        private string _expires;
+
+        public string displayName
+        {
+            get { return _displayName; }
+            set { _displayName = value; }
+        }
+
+        public string domain
+        {
+            get { return _domain; }
+            set { _domain = value; }
+        }
+
+        public string serverPort
+        {
+            get { return _serverport; }
+            set { _serverport = value; }
+        }
+
+        public string password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public string expires
+        {
+            get { return _expires; }
+            set { _expires = value; }
+        }
+
+        public string userID
+        {
+            get { return _userID; }
+            set { _userID = value; }
+        }
     }
 }
